801d06433cd726255fade455e413d66c56c38653
Olga Grigel <o.grigel83@yandex.ru>
2023-11-06 20:28:52 +0200

    Add auth

--- api/src/index.js
+++ api/src/index.js
@@ -27,7 +27,7 @@ const startServer = () => {
     const silence = new Post({name: "Silence"})
     silence.save(function(err, savedSilence) {
       if(err) return console.error(err);
-      console.log("savedSilence with olQQ", savedSilence);
+      console.log("savedSilence with ol", savedSilence);
     })
   });
 };
---
+++ auth/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { port, host, db } = require("./configuration");
+const { connectDb } = require("./helpers/db");
+
+const app = express();
+
+
+app.get("/test", (req, res) => {
+  res.send("Our auth server is working correctly");
+});
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started auth service on port ${port}`);
+    console.log(`Our host is ${host}`);
+    console.log(`Наша база данных ${db}`);
+
+  });
+};
+
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
